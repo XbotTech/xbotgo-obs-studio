74eafac8aef2510612864e6692fcf4829ccd217f
wanshun <wanshun@xbotgo.com>
2026-08-25 21:32:47 +0800

    [相机]: 添加4k分辨率相机预览流

--- plugins/xbotogo-falconM/falconm.hpp
+++ plugins/xbotogo-falconM/falconm.hpp
@@ -30,7 +30,7 @@ struct falconm_source {
 	std::string device_id = "Xbt-F-6c092e";
 	std::string broker_address = "169.254.184.18";
 	uint16_t broker_port = 1883;
-	StreamingResolution streaming_resolution = StreamingResolution::K4;
+	StreamingResolution streaming_resolution = StreamingResolution::P1080;
 	uint32_t video_ssrc = 0;
 	uint32_t audio_ssrc = 0;
 };
